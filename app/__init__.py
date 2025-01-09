b1fa659fa5b7047ff25eb6db3979f70e092bc800
Naomi Lai <nlai50@stuy.edu>
2025-01-09 11:00:01 -0500

    end of class, structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,39 @@
- 
+ # Nia Lam, Amanda Tan, Naomi Lai, Kishi Wijaya
+# Magical Magnolias
+# SoftDev
+# P02: Makers Makin' It, Act I
+# 2025-01-15
+
+# Imports
+import os
+from flask import Flask, render_template, session, redirect, request
+from database import register_user, login_user
+
+# Session
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+# Routing
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if 'username' not in session:
+        return redirect('/login')
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # If form info is from register, register the user; if info from login, user logs in
+    if request.method == 'POST':
+        if request.form.get['register']:
+            return register_user()
+        if request.form.get['login']:
+            return login_user()
+        else:
+            flash('form error')
+    return render_template('login.html')
+
+# Run
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ app/database.py
@@ -0,0 +1,11 @@
+ # Nia Lam, Amanda Tan, Naomi Lai, Kishi Wijaya
+# Magical Magnolias
+# SoftDev
+# P02: Makers Makin' It, Act I
+# 2025-01-15
+
+def register_user():
+    return 1
+
+def login_user():
+    return 1
