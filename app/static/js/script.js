cdd502d4e10d1fb04947a0c77406e67eebab5194
Naomi Lai <nlai50@stuy.edu>
2025-01-13 11:01:45 -0500

    js

---
+++ app/static/js/script.js
@@ -0,0 +1,9 @@
+// var div1 = document.getElementById('0')
+// d
+//
+// var s = document.getElementById("0");
+// s.addEventListener("click", purchase());
+//
+// var purchase = function() {
+//   console.log("purchase");
+// }
