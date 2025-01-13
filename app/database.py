e93aaaaf089877524af53a252063f8d46eda91cc
Kishi <kwijaya50@stuy.edu>
2025-01-13 10:56:40 -0500

    wa

--- app/database.py
+++ app/database.py
@@ -48,10 +48,10 @@ def init_db():
     
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS garden (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
             user TEXT UNIQUE NOT NULL,
-            flower_type TEXT UNIQUE NOT NULL,
-            days_watered INTEGER INTEGER NOT NULL,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            flower_type TEXT  NOT NULL,
+            days_watered INTEGER NOT NULL,
             grid_row INTEGER NOT NULL,
             grid_col INTEGER NOT NULL,
             max_growth INTEGER NOT NULL
@@ -60,8 +60,8 @@ def init_db():
     
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS profile (
-            flower_type TEXT UNIQUE NOT NULL,
             user TEXT UNIQUE NOT NULL,
+            flower_type TEXT UNIQUE NOT NULL,
             max_growth INTEGER NOT NULL
         )
     ''')
@@ -82,7 +82,7 @@ def database_connect():
     return conn
 
 #Flower
-def flowerbase():
+def flowerbase(user):
     if not os.path.exists('magnolia.db'):
         try:
             conn = database_connect()
@@ -97,11 +97,13 @@ def flowerbase():
                     water_req = info[4]
                     img = info[5]
                     cursor.execute('INSERT INTO flower_base (ID, flower_type, cost, max_growth, water_req, img) VALUES (?, ?, ?, ?, ?, ?)', (ID, flower_type, cost, max_growth, water_req, img))
+                    #cursor.execute('INSERT INTO seeds (user, flower_id, quantity) VALUES (?, ?, ?)', (user, ID, 0))
                 conn.commit()
         except sqlite3.IntegrityError:
             flash('Database Error')
     else:
         print("database exists")
+        
  
  #Stats
 def stats(user, magicpower, flowerscore):
@@ -124,30 +126,52 @@ def stats_edit(user, magicpower, flowerscore):
 
 
 #Garden
+def garden(user):
+    try:
+        conn = database_connect()
+        for r in range(6):
+            for c in range(6):
+                cursor = conn.cursor()
+                cursor.execute('INSERT INTO profile (user, id, flower_type, days_watered, grid_row, grid_col, max_growth) VALUES (?, ?, ?, ?, ?, ?, ?)', (user, 0, "none", 0, r, c, 0))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        flash('Database Error')
+    print("gardenrun")
+        
+def garden_add(user, r, c, ID, flower_type, max_growth):
+    try:
+        conn = database_connect()
+        cursor = conn.cursor()
+        cursor.execute('UPDATE stats SET id = ? AND flower_type = ? AND max_growth = ? WHERE user = ? AND r = ? AND c = ?', (ID, flower_type, max_growth, user, r, c))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print('Database Error')
+    print("gardenrun")
+
+        
+def garden_edit(user, r, c, ID, days_watered):
+    try:
+        conn = database_connect()
+        cursor = conn.cursor()
+        cursor.execute('UPDATE stats SET days_watered = ? WHERE user = ? AND r = ? AND c = ? AND ID = ?', (days_watered, user, r, c, ID))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print('Database Error')
+    print("gardenrun")
+
+garden("who")
 
 
 #Profile
-# def profile():
-#     if not os.path.exists('magnolia.db'):
-#         try:
-#             conn = database_connect()
-#             with open('profile.csv') as csvfile:
-#                 readn = csv.reader(csvfile)
-#                 cursor = conn.cursor()
-#                 for info in readn:
-#                     flower_type = info[0]
-#                     user = info[1]
-#                     max_growth = info[2]
-#                     max_growth = info[3]
-#                     water_req = info[4]
-#                     img = info[5]
-#                 cursor.execute('INSERT INTO profile (flower_type, user, max_growth) VALUES (?, ?, ?)', (flower_type, user, max_growth))
-#                 conn.commit()
-#         except sqlite3.IntegrityError:
-#             flash('Database Error')
-#     else:
-#         print("database exists")
- 
+def profile(user, flower_type, max_growth):
+    try:
+        conn = database_connect()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO profile (user, flower_type, max_growth) VALUES (?, ?, ?)', (user, flower_type, max_growth))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        flash('Database Error')
+
 
 # User
 def register_user():
